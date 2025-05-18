d81f18d28bddd46bbe32a1bc2a0b7fb60caafc75
Ifajul Islam <135067157+ifajul89@users.noreply.github.com>
2025-05-18 22:26:05 +0600

    Add @types/cors for TypeScript support and implement CORS middleware in the Express app. Update server to use config for port and database URL.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
 import app from "./app";
+import config from "./app/config";
+import mongoose from "mongoose";
 
-const port = 5000;
+async function startServer() {
+  try {
+    await mongoose.connect(config.databaseUrl as string);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+    app.listen(config.port, () => {
+      console.log(`Example app listening on ${config.port}`);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
